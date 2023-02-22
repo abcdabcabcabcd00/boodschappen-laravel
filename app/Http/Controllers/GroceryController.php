08a62f9eb6cd737be0412ff6f10e0872eece24b9
abcdabcabcabcd00 <117899745+abcdabcabcabcd00@users.noreply.github.com>
2023-02-22 10:22:18 +0100

    Pushed

---
+++ app/Http/Controllers/GroceryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
+
+class GroceryController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    // TODO: Add stuff
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\GroceryController as GroceryController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get   ( "/groceries",              [ GroceryController::class, "index" ] )   -> name ("groceries.index");
+Route::get   ( "/groceries/create",       [ GroceryController::class, "create" ] )  -> name ("groceries.create");
+Route::post  ( "/groceries",              [ GroceryController::class, "store" ] )   -> name ("groceries.store");
+Route::post  ( "/grocery/{grocery}/edit", [ GroceryController::class, "edit" ] )    -> name ("groceries.edit");
+Route::put   ( "/grocery/{grocery}",      [ GroceryController::class, "update" ] )  -> name ("groceries.update");
+Route::patch ( "/grocery/{grocery}",      [ GroceryController::class, "update" ] )  -> name ("groceries.update");
+Route::delete( "/grocery/{grocery}",      [ GroceryController::class, "destroy" ] ) -> name ("groceries.destroy");
 
-Route::get("/groceries", function () {
-
-});
+Route::redirect("/", "/groceries");
